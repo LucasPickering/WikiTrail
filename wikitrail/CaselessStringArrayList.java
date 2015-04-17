4dfe533e637e0b165121948a6d7fe03a1f013a31
Lucas Pickering <pickering.l@husky.neu.edu>
2015-04-17 16:08:42 -0400

    Bunch of stuff

---
+++ wikitrail/CaselessStringArrayList.java
@@ -0,0 +1,24 @@
+package wikitrail;
+
+import java.util.ArrayList;
+
+/**
+ * An {@code ArrayList} containing only strings. The only difference between this and {@code ArrayList<String>} is that
+ * {@code contains} does not consider the case of letters when comparing strings
+ */
+public class CaselessStringArrayList extends ArrayList<String> {
+
+    @Override
+    public boolean contains(Object o) {
+        for (String s : this) {
+            if (o == null) {
+                if (s == null) {
+                    return true;
+                }
+            } else if (o instanceof String && ((String) o).equalsIgnoreCase(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- wikitrail/WikiTrail.java
+++ wikitrail/WikiTrail.java
@@ -5,13 +5,53 @@ package wikitrail;
  */
 public class WikiTrail {
 
+    private final CaselessStringArrayList trail = new CaselessStringArrayList();
+
+    /* INVARIANTS
+     *  - trail is non-null
+     *  - All elements of trail are non-null
+     */
+
     public static void main(String[] args) {
+        new WikiTrail().run(args);
+    }
+
+    /**
+     * Main loop of the program.
+     *
+     * @param args arguments passed from the command line
+     */
+    private void run(String... args) {
         if (args.length == 0) {
             System.out.println("No article specified. Specify the article name with arguments.\n" +
                     "Multi-word article names can be separated by either spaces or underscores.");
-            return;
-        }
+        } else {
+            String article = getArticleFromInput(args);
+            while (true) {
+                if (trail.contains(article)) {
+                    System.out.println("Loop found! Here's the trail:");
+                    break;
+                }
 
+                trail.add(article);
+                if (article.equalsIgnoreCase("philosophy")) {
+                    break;
+                }
+
+                article = getNextArticle(article);
+            }
+            printTrail();
+        }
+    }
+
+    /**
+     * Get an article name from the given array of args. If there is more than one argument, they are appended together,
+     * in order, and separated by underscores.
+     *
+     * @param args one or more arguments to append together (non-null, length > 0, does not contain null)
+     * @return the appended article name
+     */
+    private String getArticleFromInput(String... args) {
         final StringBuilder articleNameBuilder = new StringBuilder();
         for (int i = 0; i < args.length; i++) {
             articleNameBuilder.append(args[i]);
@@ -19,8 +59,27 @@ public class WikiTrail {
                 articleNameBuilder.append("_");
             }
         }
-        String article = articleNameBuilder.toString();
+        return articleNameBuilder.toString();
+    }
 
+    /**
+     * Gets the next article in the trail, from the given article. The next article is defined as the first article
+     * linked in the given article's body that is not in parentheses or italics.
+     *
+     * @param article the first article
+     * @return the next article
+     */
+    private String getNextArticle(String article) {
+        return "Philosophy";
+    }
 
+    /**
+     * Print the current trail.
+     */
+    private void printTrail() {
+        System.out.println(trail.size() + " article(s) traversed");
+        for (int i = 0; i < trail.size(); i++) {
+            System.out.println(i + 1 + ". " + trail.get(i).replace('_', ' '));
+        }
     }
 }
