9c229cb91bc61569e07ff63a87ba42501dac7f78
Lucas Pickering <pickering.l@husky.neu.edu>
2016-04-25 23:39:07 -0400

    Switched to compiled regexes

--- wikitrail.py
+++ wikitrail.py
@@ -14,11 +14,11 @@ import re
 # Constants
 destArticle = "Philosophy"
 wikiUrl = "http://en.wikipedia.org/wiki/"
-pRegex = r'<p[^>]*>(.*?)</p>'
-spanRegex = r'<span[^>]*>(.*?)</span>'
-italicsRegex = r'<i>(.*?)</i>'
-tableRegex = r'<table[^>]*>(.*?)</table>'
-linkRegex = r'<a href="/wiki/(?!Help:)(.*?)"'
+pRegex = re.compile('<p[^>]*>(.*?)</p>', re.DOTALL)
+spanRegex = re.compile('<span[^>]*>(.*?)</span>', re.DOTALL)
+italicsRegex = re.compile('<i>(.*?)</i>', re.DOTALL)
+tableRegex = re.compile('<table[^>]*>(.*?)</table>', re.DOTALL)
+linkRegex = re.compile('<a href="/wiki/(?!Help:)(.*?)"', re.DOTALL)
 
 # Downloads and returns the html text for the wikipedia page by the given name
 def getArticleHtml(name):
@@ -29,12 +29,12 @@ def getArticleHtml(name):
 
 # Returns the name of the first article linked in the given html text, in all lower case
 def getNextArticleName(page):
-    page = re.sub(spanRegex, '', page, flags=re.DOTALL) # Strip out everything in a span tag
-    page = re.sub(tableRegex, '', page, flags=re.DOTALL) # Strip out all tables
-    page = ''.join(re.findall(pRegex, page, re.DOTALL)) # Get everything in <p> tags
-    page = re.sub(italicsRegex, '', page, flags=re.DOTALL) # Strip out everything in italics
+    page = spanRegex.sub('', page) # Strip out everything in a span tag
+    page = tableRegex.sub('', page) # Strip out all tables
+    page = pRegex.search(page).group(0) # Get everything in <p> tags
+    page = italicsRegex.sub('', page) # Strip out everything in italics
     page = stripParens(page) # Strip out everything in parentheses
-    page = re.findall(linkRegex, page, re.DOTALL)[0] # Get the first wiki link
+    page = linkRegex.search(page).group(1) # Get the first wiki link
     return page
 
 # Strip out everything inside parentheses
