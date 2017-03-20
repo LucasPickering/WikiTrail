3c142276c4684c85c0be66400e3526bc4d7b3db8
Lucas Pickering <pickering.l@husky.neu.edu>
2017-03-20 14:56:35 -0400

    New Trail class

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env PYTHON3
+#!/usr/bin/env python3
 
-import os
-import sys
 import wikitrail
 
 TEST_ARTICLES = ['Rose', 'Pokemon', 'America', 'Kitchen', 'Barack_Obama']
 
 
-def main():
-    print("Running tests. . .")
-    wikitrail.main(TEST_ARTICLES)
+def run_tests():
+    print("Running tests...")
+    wikitrail.main(*TEST_ARTICLES)
     print("Finished running tests")
 
+
 if __name__ == '__main__':
-    main()
+    run_tests()
--- wikitrail.py
+++ wikitrail.py
@@ -7,17 +7,15 @@
 
 import argparse
 import json
+import logging
 import os
 import re
-import sys
+import requests
 import traceback
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from operator import xor
 
 # Constants
 DEFAULT_DEST = 'Philosophy'
-wikiUrl = 'http://en.wikipedia.org/wiki/'
+WIKIPEDIA_URL = 'http://en.wikipedia.org/wiki/{}'
 SPAN_RGX = re.compile(r'<span[^>]*>.*?</span>', re.DOTALL)
 TABLE_RGX = re.compile(r'<table[^>]*>.*?</table>', re.DOTALL)
 P_RGX = re.compile(r'<p[^>]*>.*?</p>', re.DOTALL)
@@ -25,17 +23,33 @@ ITALICS_RGX = re.compile(r'<i>.*?</i>', re.DOTALL)
 LINK_RGX = re.compile(r'<a href="/wiki/(?!Help:)(.*?)"', re.DOTALL)
 JSON_FILE = 'trails.json'
 
+logging.basicConfig(format='{asctime} - {levelname} - {message}', datefmt='%Y-%m-%d %H:%M:%S',
+                    style='{')
+
+
+class Trail(list):
+
+    def __init__(self, starting_article):
+        self.append(starting_article)
+
+    def __str__(self):
+        return '\n'.join(['{}. {}'.format(i, article) for i, article in enumerate(self, 1)])
+
 
 # Downloads and returns the html text for the wikipedia page by the given name
 def download_article(name):
-    try:
-        return urlopen(wikiUrl + name).read().decode('cp1252', 'ignore')
-    except HTTPError as err:
-        print("Error loading page {} - code {}".format(name, err.code), file=sys.stderr)
+    url = WIKIPEDIA_URL.format(name)
+    response = requests.get(url)
+    if response.ok:
+        return response.text
+    logging.error("Code {} loading '{}'".format(response.text, url))
 
 
-# Returns the name of the first article linked in the given html text, in all lower case
-def get_next_article_name(text):
+def extract_next_article(text):
+    """
+    Extracts the name of the first article linked in the given HTML that fits our criteria, and
+    returns it.
+    """
     text = SPAN_RGX.sub('', text)  # Strip out everything in a span tag
     text = TABLE_RGX.sub('', text)  # Strip out all tables
     text = ''.join(P_RGX.findall(text))  # Get everything in <p> tags
@@ -45,52 +59,38 @@ def get_next_article_name(text):
     return text
 
 
-# Strip out everything inside parentheses, but not things in parentheses inside <a.../a>
+# Strip out everything inside parentheses, but not things in parentheses inside quotes
 def strip_parens(s):
-    inQuotes = False
-    inParens = False
+    in_quotes = False
+    in_parens = False
     result = ''
 
     for c in s:  # For each character in the string...
-        inQuotes = xor(inQuotes, c == '"')  # Toggle whether or not we're in quotes
-        inParens = (inParens or c == '(') and c != ')'  # Track whether or not we're in parens
-        if inQuotes or not inParens:  # If we're in quotes or not in parens...
+        in_quotes ^= c == '"'  # Toggle in_quotes if the character is a quote
+        in_parens = (in_parens or c == '(') and c != ')'  # Track whether or not we're in parens
+        if in_quotes or not in_parens:  # If we're in quotes or not in parens...
             result += c  # Add this character to the result
 
     return result
 
 
-def print_trail(trail):
-    for i, step in enumerate(trail, 1):
-        print("{}. {}".format(i, step))
-    print('')
-
-
 def trace_article(article, dest):
-    trail = [article]  # Initialize a list to track the trail
+    trail = Trail(article)  # Initialize a Trail on the starting article
     while article != dest:  # While we haven't reached the destination...
-        articleText = download_article(article)  # Get the text of the article body
+        text = download_article(article)  # Get the text of the article body
+        article = extract_next_article(text)  # Get the next article out of the article text
 
-        # If the text is None, it failed to download, so stop the trail here
-        if not articleText:
-            return None
+        if article in trail:  # If the most recent article is already in the trail...
+            # Print error and break out (the trail will still get returned and pretty printed)
+            logging.warning("Found duplicate link to: " + article)
+            break
 
-        article = get_next_article_name(articleText)  # Get the next article out of the article text
         trail.append(article)  # Add this article to the trail
 
-        if article in trail[:-1]:  # If the most recent article is already in the trail...
-            # Print error and break out (the trail will still get returned and pretty)
-            print("Found duplicate link to: " + article)
-            break
     return trail
 
 
-def main(args=None):
-    parser = argparse.ArgumentParser(description="Get the trail of a Wikipedia article")
-    parser.add_argument('start', nargs='+', help="Article(s) to start from (1 trail per entry)")
-    parser.add_argument('--dest', '-d', default=DEFAULT_DEST, help="Destination article")
-    args = parser.parse_args(args)
-
+def main(*start_articles, dest_article=DEFAULT_DEST):
     # Load previous results from JSON into a dict, or if they don't exist, make an empty dict
     if os.path.isfile(JSON_FILE):
         with open(JSON_FILE) as f:
@@ -98,21 +98,23 @@ def main(args=None):
     else:
         all_trails = {}
 
-    for article in args.start:
-        print("Tracing {}. . .".format(article))
-        sys.stdout.flush()  # Flush to make sure it gets printed before it starts trailblazing
+    for article in start_articles:
+        logging.info("Tracing {}. . .".format(article))
         try:
-            trail = trace_article(article, args.dest)  # Get the trail
-            print_trail(trail)  # Print the trail
+            trail = trace_article(article, dest_article)  # Get the trail
+            logging.info(trail)  # Print the trail
             all_trails[article] = trail  # Save the trail to the dict
-        except Exception as e:
-            print("Error from root article {}:".format(article), file=sys.stderr)
-            sys.stderr.flush()
-            traceback.print_exc(file=sys.stderr)
+        except Exception:
+            logging.error("Error from root article {}:\n{}".format(article, traceback.format_exc()))
 
     # Save all the data to the JSON file
     with open(JSON_FILE, 'w') as f:
         json.dump(all_trails, f, indent=4, sort_keys=True)
 
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Get the trail of a Wikipedia article")
+    parser.add_argument('start', nargs='+', help="Article(s) to start from (1 trail per entry)")
+    parser.add_argument('--dest', '-d', default=DEFAULT_DEST, help="Destination article")
+    args = parser.parse_args()
+    main(*args.start, args.dest)
